33b0206b6733393d24f3296edc7925f3218a607d
preciousdsmith <precious.d.smith@gmail.com>
2024-10-10 14:30:07 -0400

    uploaded to GitHub

---
+++ script.js
@@ -0,0 +1,8 @@
+function userSearch() {
+    let input = document.getElementById("user-search").value;
+    console.log('hello')
+    alert(input)
+    }
+    
+    console.log(input, "<<<<<input");
+    
